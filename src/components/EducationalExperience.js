f099ce099b1be143ac5cbdc7482a7b0b52ae0c03
Andrew Smith <meuzishun@gmail.com>
2022-05-16 09:57:54 -0400

    Add components for EducationalExperience
    I'm not 100% sure what I was doing wrong here. If I remember correctly, I forgot to bind the handleSubmission method which caused an error with setState. This was particularly confusing but once I saw it, I was like "a-duh".
    
    I also forgot to change the prop references in the display of content (not the forms) which was once everything else seemed to be working.
    
    So, a couple lessons to learn here...

--- src/components/EducationalExperience.js
+++ src/components/EducationalExperience.js
@@ -1,16 +1,95 @@
 import React, { Component } from 'react';
+import uniqid from 'uniqid';
 
 class EducationalExperience extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      education: [],
+    };
+
+    this.addEducation = this.addEducation.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+  }
+
+  addEducation() {
+    const institutions = this.state.education;
+    const institution = {
+      schoolName: '',
+      studyTitle: '',
+      graduationDate: '',
+      key: uniqid(),
+      edit: true,
+    };
+    this.setState({
+      education: [...institutions, institution],
+    });
+  }
+
+  handleSubmission(e) {
+    e.preventDefault();
+    const id = e.target.id;
+    console.log(this.state);
+    this.setState({
+      education: this.state.education.map((institute) => {
+        if (institute.key === id) {
+          institute.schoolName = e.target[0].value;
+          institute.studyTitle = e.target[1].value;
+          institute.graduationDate = e.target[2].value;
+          institute.edit = false;
+        }
+        return institute;
+      }),
+    });
+    setTimeout(() => {
+      console.log(this.state);
+    }, 1000);
+  }
+
+  handleEdit(e) {
+    e.preventDefault();
+    const id = e.target.parentElement.id;
+    this.setState({
+      education: this.state.education.map((institute) => {
+        if (institute.key === id) {
+          institute.edit = true;
+        }
+        return institute;
+      }),
+    });
   }
 
   render() {
     return (
       <div>
         <h2>Educational Experience</h2>
+        {this.state.education.map((institute) => {
+          return institute.edit ? (
+            <form
+              key={institute.key}
+              id={institute.key}
+              onSubmit={this.handleSubmission}
+            >
+              <label>School Name: </label>
+              <input defaultValue={institute.schoolName} />
+              <label>Title of Study: </label>
+              <input defaultValue={institute.studyTitle} />
+              <label>Graduation Data: </label>
+              <input defaultValue={institute.graduationDate} />
+              <button type='submit'>Submit</button>
+            </form>
+          ) : (
+            <div key={institute.key} id={institute.key}>
+              <p>School Name: {institute.schoolName}</p>
+              <p>Title of Study: {institute.studyTitle}</p>
+              <p>Graduation Data: {institute.graduationDate}</p>
+              <button onClick={this.handleEdit}>Edit</button>
+            </div>
+          );
+        })}
+        <button onClick={this.addEducation}>Add</button>
       </div>
     );
   }
--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -5,13 +5,67 @@ class GeneralInformation extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      name: '',
+      address: '',
+      phone: '',
+      email: '',
+      edit: true,
+    };
+
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleSubmission = this.handleSubmission.bind(this);
+  }
+
+  handleEdit() {
+    console.log(this.state);
+    this.setState({
+      name: this.state.name,
+      address: this.state.address,
+      phone: this.state.phone,
+      email: this.state.email,
+      edit: true,
+    });
+    console.log(this.state);
+  }
+
+  handleSubmission(e) {
+    e.preventDefault();
+    const form = e.target;
+    this.setState({
+      name: form[0].value,
+      address: form[1].value,
+      phone: form[2].value,
+      email: form[3].value,
+      edit: false,
+    });
   }
 
   render() {
     return (
       <div>
         <h2 className='info-header'>General Information</h2>
+        {this.state.edit ? (
+          <form onSubmit={this.handleSubmission}>
+            <label>Name: </label>
+            <input defaultValue={this.state.name} />
+            <label>Address: </label>
+            <input defaultValue={this.state.address} />
+            <label>phone: </label>
+            <input defaultValue={this.state.phone} />
+            <label>email: </label>
+            <input defaultValue={this.state.email} />
+            <button type='submit'>Submit</button>
+          </form>
+        ) : (
+          <div>
+            <p>Name: {this.state.name}</p>
+            <p>Address: {this.state.address}</p>
+            <p>Phone: {this.state.phone}</p>
+            <p>Email: {this.state.email}</p>
+            <button onClick={this.handleEdit}>Edit</button>
+          </div>
+        )}
       </div>
     );
   }
