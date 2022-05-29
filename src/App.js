cc3a14a0b3d3ef9c4d7b47ac568e9a6e41c785d0
Andrew Smith <meuzishun@gmail.com>
2022-05-29 18:03:53 -0400

    Remove old code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react';
 import React from 'react';
 import Header from './components/Header';
 import GeneralInformation from './components/GeneralInformation';
@@ -6,27 +5,6 @@ import EducationalExperience from './components/EducationalExperience';
 import PracticalExperience from './components/PracticalExperience';
 //TODO: Import styles here like below
 import './styles/App.css';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {};
-//   }
-
-//   render() {
-//     return (
-//       <div className='App'>
-//         <Header />
-//         <main className='main-container'>
-//           <GeneralInformation />
-//           <EducationalExperience />
-//           <PracticalExperience />
-//         </main>
-//       </div>
-//     );
-//   }
-// }
 
 function App() {
   return (
